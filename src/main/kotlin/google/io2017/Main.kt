c955c886e69cc8e50cb0eb5d6e268096d5e157c3
N Pichaiwattnphon <nata8ify@gmail.com>
2018-01-15 15:59:25 +0700

    Null safety, Type extension, Gson, Raw file reader, Higher-order function

--- src/main/kotlin/google/io2017/Main.kt
+++ src/main/kotlin/google/io2017/Main.kt
@@ -1,5 +1,9 @@
 package google.io2017
 
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import rest.client.Post
+import java.io.FileReader
 import java.math.BigDecimal
 
 fun main(args: Array<String>){
@@ -16,10 +20,36 @@ fun main(args: Array<String>){
     val instantBD : BigDecimal = 100000000.bd
     println("Is instantBD ($instantBD) a BigDecimal?... ${instantBD is BigDecimal}")
 
+    var nullStr : String? = null
+    var notNullStr : String? = "Kotlin"
+    println("nullStr? ${nullStr?.length }")
+    println("notNullStr!! ${notNullStr!!.length}")
 
+
+    var moneys = listOf(Money(506000, "YEN"), Money(100, "RUP"), Money(8900, "BATH"), Money(1000, "BATH"), Money(256, "YEN"), Money(currency = "BATH", balance = 1566))
+    var predicateCurrency : (String) -> (Boolean) = {currency -> currency.contains("BATH")}
+    println(exHighOrderFunctionForNoBath(moneys, predicateCurrency))
+
+
+    var yenMoneys = moneys.filter {it.currency.contains("YEN")}
+            .sortedBy { it.balance }
+    println(yenMoneys)
+
+    var (_, currency) = yenMoneys.first()
+    println("$currency")
+
+    var readJson = FileReader("posts.json")
+    var posts : List<Post> = Gson().fromJson(readJson, object : TypeToken<List<Post>>(){}.type)
+    //println("Post from raw file : $posts")
 }
 
-private val Int.bd: BigDecimal
-    get() {
-        return this.toBigDecimal()
-    }
+fun exHighOrderFunctionForNoBath(moneys : List<Money>, predicateCurrency : (String) -> (Boolean)) : List<Money>? {
+    var mutableMoneys =  moneys.toMutableList()
+    moneys.forEach { if(predicateCurrency(it.currency)){
+        println(it)
+        mutableMoneys.remove(it)
+    } }
+    return mutableMoneys
+}
+
+private val Int.bd: BigDecimal get() { return this.toBigDecimal()}
